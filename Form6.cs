8e137c24d543fa10034f692d2e92288dbdebcb39
SergeiKalabaev <46010439+SergeiKalabaev@users.noreply.github.com>
2018-12-20 04:36:41 +0400

    Добавление LoadData

--- Form6.cs
+++ Form6.cs
@@ -20,7 +20,30 @@ namespace WindowsFormsApp1
            
         }
 
-  
+        private void LoadData()
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\Kys\source\repos\WindowsFormsApp1\WindowsFormsApp1\Database1.mdf;Integrated Security=True;User Instance=True";
+            SqlConnection Connection = new SqlConnection(connectionString);
+            Connection.Open();
+            string query = "SELECT * FROM Users ORDER BY Id";
+            SqlCommand command = new SqlCommand(query, Connection);
+            SqlDataReader reader = command.ExecuteReader();
+            List<string[]> data = new List<string[]>();
+            while (reader.Read())
+            {
+                data.Add(new string[4]);
+                data[data.Count - 1][0] = reader[0].ToString();
+                data[data.Count - 1][1] = reader[3].ToString();
+                data[data.Count - 1][2] = reader[4].ToString();
+                data[data.Count - 1][3] = reader[6].ToString();
+            }
+            reader.Close();
+
+            Connection.Close();
+
+            foreach (string[] s in data)
+                dataGridView1.Rows.Add(s);
+        }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
